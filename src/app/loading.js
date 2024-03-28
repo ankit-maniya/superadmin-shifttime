46b281c1c3b6720a6fd7f31a144f1df8a9b0e854
Ankit Maniya <66209504+ankit-maniya@users.noreply.github.com>
2024-03-28 02:34:23 -0400

    loading image added for better view

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,4 +1,11 @@
+import Image from "next/image";
+
 export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
-    return <div>Loading from Home...</div>;
-  }
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div className="relative flex justify-center items-cente h-full">
+      <div className="absolute animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-purple-500"></div>
+      <Image src="/images/ShiftTimeIcon.png" width="100" height="100" className="rounded-full h-28 w-28" alt="shifttime logo"/>
+    </div>
+  );
+}
