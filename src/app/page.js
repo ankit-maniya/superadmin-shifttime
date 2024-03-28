614024483ae9ce1b95be0001912d8788c4f0fc18
Ankit Maniya <66209504+ankit-maniya@users.noreply.github.com>
2024-03-27 22:35:44 -0400

    Fixies Typo Error

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Hero from "../components/Hero";
 import Features from "../components/Features";
 import FeaturesBlocks from "../components/FeaturesBlocks";
 import Testimonials from "../components/Testimonials";
-import Newsletter from "../components/Newsletter";
+import Newsletter from "../components/NewsLetter";
 
 export default function Home() {
   return (
